d21ad561f3d84f6ce6736d2c74ac4f7bde6fc7a5
Luuk van der Duim <luukvanderduim@gmail.com>
2022-12-13 00:35:16 +0100

    fix dependency, try things out, still fail.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,57 @@
-use std::error::Error;
-
 use atspi::zbus::MatchRule;
+use std::error::Error;
 use tokio_stream::StreamExt;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() -> Result<(), Box<dyn Error>> {
     // set a11y on session bus
     atspi::set_session_accessibility(true).await?;
-
-    if let Err(e) = atspi::Connection::open().await {
-        panic!("Could not connect to Registry daemon: {e:?}");
-    }
+    // Open connection
     let registry = atspi::Connection::open().await?;
 
-    if let Err(e) = registry.register_event("Cache:AddAccessible").await {
+    let caret_ev = "Object:TextCaretMoved:";
+
+    let registry_evs = [
+        "Registry:EventListenerRegistered",
+        "Registry:EventListenerRegistered",
+    ];
+    let cache_evs = ["Cache:AddAccessible", "Cache:RemoveAccessible"];
+    let socket_ev = "Socket::Available";
+
+    for ev in registry_evs {
+        if let Err(e) = registry.register_event(ev).await {
+            panic!("Could not register event: {e:?}");
+        }
+    }
+    for ev in cache_evs {
+        if let Err(e) = registry.register_event(ev).await {
+            panic!("Could not register event: {e:?}");
+        }
+    }
+    if let Err(e) = registry.register_event(socket_ev).await {
         panic!("Could not register event: {e:?}");
     }
-
-    if let Err(e) = registry.register_event("Cache:RemoveAccessible").await {
+    if let Err(e) = registry.register_event(caret_ev).await {
         panic!("Could not register event: {e:?}");
     }
 
     let rule_add = MatchRule::builder()
         .interface("org.a11y.atspi.Cache")?
-        .member("AddAccessible")?
         .build();
     let rule_rem = MatchRule::builder()
         .interface("org.a11y.atspi.Cache")?
         .member("RemoveAccessible")?
+        .build();
+    let rule_avail = MatchRule::builder()
+        .interface("org.a11y.atspi.Socket")?
+        .build();
+
+    let rule_reg = MatchRule::builder()
+        .interface("org.a11y.atspi.Registry")?
+        .build();
+
+    let rule_mse = MatchRule::builder()
+        .interface("org.a11y.atspi.Event.Object.TextCaretMoved")?
         .build();
 
     let a11y_bus = atspi::Connection::open().await?;
@@ -35,15 +59,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // For the FreeDesktop Org. primary bus fnctionality
     let dbus_proxy = atspi::zbus::fdo::DBusProxy::new(a11y_dbus_connection).await?;
+    println!("DBus Proxy path: {}", dbus_proxy.path());
 
     dbus_proxy.add_match_rule(rule_add).await?;
     dbus_proxy.add_match_rule(rule_rem).await?;
+    dbus_proxy.add_match_rule(rule_reg).await?;
+    dbus_proxy.add_match_rule(rule_avail).await?;
+    dbus_proxy.add_match_rule(rule_mse).await?;
 
-    let registry_deamon_stream = registry.event_stream();
+    let mut registry_daemon_stream = registry.event_stream();
 
-    tokio::pin!(registry_deamon_stream);
+    tokio::pin!(registry_daemon_stream);
 
-    while let Some(ev) = registry_deamon_stream.next().await {
+    while let Some(ev) = (&mut registry_daemon_stream).next().await {
         match ev {
             Ok(ev) => println!("My Precious: {}", ev.event_string()),
             _ => println!("Error on stream -- "),
