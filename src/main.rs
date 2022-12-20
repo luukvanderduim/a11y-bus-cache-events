4362dc425144c35c56b82b771ab33a5ed955b9d5
Luuk van der Duim <luukvanderduim@gmail.com>
2022-12-20 23:33:42 +0100

    ported to folly-typed-events

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,57 @@
-use atspi::zbus::{
-  MatchRule,
-  MessageType,
-};
+use atspi::events::{CacheEvent, Event};
+
+use atspi::identify::ButtonEvent;
+use atspi::zbus::{fdo::DBusProxy, MatchRule, MessageType};
 use std::error::Error;
 use tokio_stream::StreamExt;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> Result<(), Box<dyn Error>> {
     // set a11y on session bus
     atspi::set_session_accessibility(true).await?;
     // Open connection
-    let registry = atspi::Connection::open().await?;
+    let atspi = atspi::Connection::open().await?;
 
-    let caret_ev = "Object:TextCaretMoved:";
+    atspi.register_event("Cache:Add").await?;
+    atspi.register_event("Cache:Remove").await?;
+    atspi.register_event("Mouse:Button").await?;
 
-    let registry_evs = [
-        "Registry:EventListenerRegistered",
-        "Registry:EventListenerRegistered",
-    ];
-    let cache_evs = ["Cache:AddAccessible", "Cache:RemoveAccessible"];
-    let socket_ev = "Socket::Available";
+    let mouse_rule = MatchRule::builder()
+        .msg_type(MessageType::Signal)
+        .interface("org.a11y.atspi.Event.Mouse")?
+        .build();
 
-    for ev in registry_evs {
-        if let Err(e) = registry.register_event(ev).await {
-            panic!("Could not register event: {e:?}");
-        }
-    }
-    for ev in cache_evs {
-        if let Err(e) = registry.register_event(ev).await {
-            panic!("Could not register event: {e:?}");
-        }
-    }
-    if let Err(e) = registry.register_event(socket_ev).await {
-        panic!("Could not register event: {e:?}");
-    }
-    if let Err(e) = registry.register_event(caret_ev).await {
-        panic!("Could not register event: {e:?}");
-    }
-
-    let rule_rem = MatchRule::builder()
+    let cache_rule = MatchRule::builder()
         .msg_type(MessageType::Signal)
         .interface("org.a11y.atspi.Cache")?
-        .member("RemoveAccessible")?
-        .build();
-    let rule_add = MatchRule::builder()
-        .msg_type(MessageType::Signal)
-        .interface("org.a11y.atspi.Cache")?
-        .member("AddAccessible")?
-        .build();
-    let rule_avail = MatchRule::builder()
-        .msg_type(MessageType::Signal)
-        .interface("org.a11y.atspi.Socket")?
         .build();
 
-    let rule_reg = MatchRule::builder()
-        .msg_type(MessageType::Signal)
-        .interface("org.a11y.atspi.Registry")?
-        .build();
+    let dbus_proxy = DBusProxy::new(atspi.connection()).await?;
+    dbus_proxy.add_match_rule(mouse_rule).await?;
+    let dbus_proxy = DBusProxy::new(atspi.connection()).await?;
+    dbus_proxy.add_match_rule(cache_rule).await?;
 
-    let rule_mse = MatchRule::builder()
-        .msg_type(MessageType::Signal)
-        .interface("org.a11y.atspi.Event.Object")?
-        .member("TextCaretMoved")?
-        .build();
+    let event_stream = atspi.event_stream();
 
-    // For the FreeDesktop Org. primary bus fnctionality
-    let dbus_proxy = atspi::zbus::fdo::DBusProxy::new(registry.connection()).await?;
-    println!("DBus Proxy path: {}", dbus_proxy.path());
-
-    dbus_proxy.add_match_rule(rule_add).await?;
-    dbus_proxy.add_match_rule(rule_rem).await?;
-    dbus_proxy.add_match_rule(rule_reg).await?;
-    dbus_proxy.add_match_rule(rule_avail).await?;
-    dbus_proxy.add_match_rule(rule_mse).await?;
-
-    let registry_daemon_stream = registry.event_stream();
-
-    tokio::pin!(registry_daemon_stream);
-    while let Some(ev) = registry_daemon_stream.next().await {
+    tokio::pin!(event_stream);
+    while let Some(ev) = event_stream.next().await {
         match ev {
-            Ok(ev) => println!("My Precious: {}", ev.event_string()),
-            Err(e) => println!("Error on stream -- {:#?}", e),
+            Ok(ev) => match ev {
+                Event::Atspi(aev) => {
+                    let button_ev = ButtonEvent::try_from(aev)?;
+                    println!(
+                        "The {} button press at: {},{}",
+                        button_ev.button(),
+                        button_ev.x(),
+                        button_ev.y()
+                    );
+                }
+                Event::Cache(cev) => match cev {
+                    CacheEvent::Add(caev) => println!("Cache Add event: {caev:?}"),
+                    CacheEvent::Remove(crev) => println!("Cache remove: {crev:?}"),
+                },
+            },
+            Err(e) => println!("Error on stream -- {e:#?}"),
         };
     }
 
