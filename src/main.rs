a19fdf1a202535786d6c2ddaf522d2d646015a0b
Luuk van der Duim <luukvanderduim@gmail.com>
2022-12-27 23:47:54 +0100

    updated to catch up with fully-typed-events

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use atspi::cache::CacheItem;
-use atspi::events::{Accessible, CacheEvent, Event, GenericEvent};
+use atspi::events::{Accessible, CacheEvent, Event, EventInterfaces, GenericEvent};
 
-use atspi::identify::{ButtonEvent, DocumentEvents};
+use atspi::identify::{ButtonEvent, DocumentEvents, MouseEvents, WindowEvents};
 use atspi::zbus::{fdo::DBusProxy, MatchRule, MessageType};
 use std::error::Error;
 use tokio_stream::StreamExt;
@@ -14,18 +14,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let atspi = atspi::Connection::open().await?;
 
-    atspi.register_event("Cache:Add").await?;
-    atspi.register_event("Cache:Remove").await?;
-    atspi.register_event("Mouse:Button").await?;
+    // atspi.register_event("Cache:Add").await?;
+    // atspi.register_event("Cache:Remove").await?;
+    atspi.register_event("Mouse").await?;
     atspi.register_event("Document").await?;
+    atspi.register_event("Window").await?;
 
     let mouse_rule = MatchRule::builder()
         .msg_type(MessageType::Signal)
         .interface("org.a11y.atspi.Event.Mouse")?
-        .member("Button")?
         .build();
 
-    let cache_rule = MatchRule::builder()
+    let _cache_rule = MatchRule::builder()
         .msg_type(MessageType::Signal)
         .interface("org.a11y.atspi.Cache")?
         .build();
@@ -35,47 +35,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .interface("org.a11y.atspi.Event.Document")?
         .build();
 
+    let window_rule = MatchRule::builder()
+        .msg_type(MessageType::Signal)
+        .interface("org.a11y.atspi.Event.Window")?
+        .build();
+
     let dbus_proxy = DBusProxy::new(atspi.connection()).await?;
-    dbus_proxy.add_match_rule(mouse_rule).await?;
-    dbus_proxy.add_match_rule(cache_rule).await?;
+    //dbus_proxy.add_match_rule(mouse_rule).await?;
+    // dbus_proxy.add_match_rule(cache_rule).await?;
     dbus_proxy.add_match_rule(document_rule).await?;
+    dbus_proxy.add_match_rule(window_rule).await?;
 
     let event_stream = atspi.event_stream();
 
     tokio::pin!(event_stream);
-    while let Some(ev) = event_stream.next().await {
-        match ev {
-            Ok(ev) => match ev {
-                Event::Atspi(aev) => {
-                    if aev.member().unwrap().as_str() == "Document" {
-                        let doc_ev = DocumentEvents::from(aev.clone());
-                        {
-                            println!("What happened to the doc? {doc_ev:#?}");
-                        }
-                    }
-
-                    let button_ev = ButtonEvent::try_from(aev)?;
-                    println!(
-                        "Button: {} at: {},{}",
-                        button_ev.button(),
-                        button_ev.x(),
-                        button_ev.y()
-                    );
-                }
-                Event::Cache(cev) => match cev {
-                    CacheEvent::Add(caev) => {
-                        let item: &CacheItem = caev.item();
-                        println!("CacheItem:  {item:#?}")
-                    }
-                    CacheEvent::Remove(crev) => {
-                        let acc: &Accessible = crev.as_accessible();
-                        println!("Removed Accessible: {acc:?}");
-                    }
-                },
-                _ => println!("Unmatched event"),
-            },
-
-            Err(e) => println!("Error on stream -- {e:#?}"),
+    while let ev = event_stream.next().await.expect("None") {
+        match ev? {
+            Event::Interfaces(EventInterfaces::Mouse(mse)) => {
+                println!("Mouse event ");
+                //    println!("mse    {mse:#?}");
+            }
+            Event::Interfaces(EventInterfaces::Window(win)) => {
+                println!("win event ");
+                println!("mse    {win:?}");
+            }
+            Event::Interfaces(EventInterfaces::Object(obj)) => {
+                println!("win event ");
+                println!("object    {obj:?}");
+            }
+            _ => println!("other event "),
         };
     }
 
